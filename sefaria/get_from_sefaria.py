64b03f94a3d559b4c84c9236ec31836ddc3e4214
zevisvei <0583238037h@gmail.com>
2025-01-13 04:11:00 +0200

    c

--- sefaria/get_from_sefaria.py
+++ sefaria/get_from_sefaria.py
@@ -5,7 +5,8 @@ class Book:
     def __init__(
             self, book_title: str, lang: str,
             he_title: str|None = None,
-            categories: list|None = None
+            categories: list|None = None,
+            get_links: list|bool = False
             ) -> None:
         self.book_title = book_title
         self.metadata = {}
@@ -16,9 +17,10 @@ class Book:
         self.he_title = he_title
         self.categories = categories
         self.shape = self.sefaria_api.get_shape(self.book_title)
-        self.iscomplex = bool(self.shape[0].get("isComplex"))
+        self.is_complex = bool(self.shape[0].get("isComplex"))
         self.index = self.sefaria_api.get_index(self.book_title)
-        self.iscomplex = bool(self.index["schema"].get("nodes"))
+        self.is_complex_and_simple = bool(self.is_complex !=  bool(self.index["schema"].get("nodes")))
+        self.get_links = get_links
 
     def get_metadata(self) -> dict[str, str]:
         era_dict = {
@@ -27,7 +29,7 @@ class Book:
              "AH" : {"en":"Achronim", "he": "אחרונים"},
              "T" : {"en":"Tannaim", "he": "תנאים"},
              "T" : {"en":"Amoraim", "he": "אמוראים"},
-             "CO" : {"en":"Contemporary", "he": "?"}
+             "CO" : {"en":"Contemporary", "he": "מחברי זמננו"}
             }
         authors = self.index.get("authors")
         if authors:
@@ -53,23 +55,40 @@ class Book:
         return self.metadata
 
     def process_book(self) -> list:
-        if not self.iscomplex:
-            self.process_simple_book()
-        else:
+        if self.is_complex:
             self.node_num = 0
             self.process_node(self.index["schema"])
+        elif self.is_complex_and_simple:
+            self.node_num = 0
+            self.process_complex_and_simple_book(self.index["schema"])
+        else:
+            self.process_simple_book()
         return self.book_content
     
+    def process_complex_and_simple_book(self, node: dict, level: int=1) -> None:
+        section_names = self.sefaria_api.get_name(self.book_title)["heSectionNames"] if self.lang == "he" else  self.sefaria_api.get_name(self.book_title)["sectionNames"] 
+        node = node['nodes'][0]
+        depth = node['depth'] +1
+        key = [self.book_title, node["key"]]
+        node_len = self.shape[0]["length"]
+        for num in range(1, node_len +1):
+            key.append(str(num))
+            node_index = ", ".join(key)
+            text = self.sefaria_api.get_book(node_index, self.long_lang)
+            text = text.get("versions")
+            if text:
+                self.recursive_sections(section_names, text[0]['text'], depth-1, level+1)
+            key.pop()
+
     def process_simple_book(self) -> None:
         index = self.index
         if self.lang == "he":
             section_names = self.sefaria_api.get_name(self.book_title).get("heSectionNames")
         else:
-            section_names = "" #to do
+            section_names = self.sefaria_api.get_name(self.book_title).get("sectionNames")
         depth = index['schema']['depth']
         text = self.sefaria_api.get_book(self.book_title, self.long_lang)
         # add book title
-        self.book_content.append(f'<h1>{self.he_title}</h1>\n')
         # add authors name
         #if index.get("authors"):
         #    for author in index['authors']:
@@ -120,13 +139,13 @@ class Book:
                 for num in range(1, node_len +1):
                     key.append(str(num))
                     node_index = ", ".join(key)
-                    section_names = self.sefaria_api.get_name(node_index)["heSectionNames"] if self.lang == "he" else "" #to do
+                    section_names = self.sefaria_api.get_name(node_index)["heSectionNames"] if self.lang == "he" else  self.sefaria_api.get_name(node_index)["sectionNames"] 
                     section_name = section_names[0]
                     text = self.sefaria_api.get_book(node_index, self.long_lang)
                     text = text.get("versions")
                     if text:
                         self.book_content.append(
-                        f"<h{min(level, 6)}>{section_name} {to_gematria(num)}</h{min(level, 6)}>\n"
+                            f"<h{min(level, 6)}>{section_name} {to_gematria(num)}</h{min(level, 6)}>\n"
                         )
                         self.recursive_sections(section_names, text[0]['text'], depth-1, level+1)
                     
@@ -150,8 +169,11 @@ class Book:
             self.node_num += 1
 
     def recursive_sections(
-            self, section_names: list, text: list, depth: int,level: int=0
+            self, section_names: list|None, text: list, depth: int,level: int=0, add_letter: str = ""
             ) -> None:
+
+            skip_section_names = ('שורה', 'פירוש', 'פסקה', 'Line', 'Comment', 'Paragraph')
+            letter_to_add = ""
             """
             Recursively generates section names based on depth and appends to output list.
             :param section_names: list of section names
@@ -160,13 +182,16 @@ class Book:
             :return: None
             """
             if depth == 0 and text != [] and not isinstance(text, bool):
-                self.book_content.append(f"<p>{text}</p>")
+                self.book_content.append(f"<p>{add_letter}{text}</p>")
             elif not isinstance(text, bool):
                 for i, item in enumerate(text, start=1):
                     if item != [] and item != [[]]:
-                        letter = to_daf(i) if section_names[-depth] == 'דף' else to_gematria(i)
-                        if depth>1:                        
+                        letter = to_daf(i) if section_names and section_names[-depth] == 'דף' else to_gematria(i)
+                        if depth>1 and section_names:                        
                             self.book_content.append(f"<h{min(level, 6)}>{section_names[-depth]} {letter}</h{min(level, 6)}>\n")
-                        elif section_names[-depth] != 'שורה' and section_names[-depth] != 'פירוש' and section_names[-depth] != 'פסקה':
-                            self.book_content.append(f'({letter}) ')         
-                    self.recursive_sections(section_names, item, depth-1,level+1)
+                        elif section_names and section_names[-depth] not in skip_section_names:
+                            letter_to_add = f"<b>{letter}</b> "
+                    self.recursive_sections(section_names, item, depth-1,level+1, letter_to_add)
+
+    def get_links_content(self, book_title: str):
+        links = self.sefaria_api.get_links(book_title)
--- test.py
+++ test.py
@@ -17,17 +17,19 @@ for file in os.listdir(path):
             --comments
 
 """
-def to_ebook(input_file: str, output_file: str, dict_arga: dict[str, str],
+def to_ebook(input_file: str, output_file: str, dict_args: dict[str, str],
             level1_toc: str="//h:h1", level2_toc: str="//h:h2", level3_toc: str="//h:h3"):
     args = ["ebook-convert",input_file, output_file,
                     f"--level1-toc={level1_toc}" ,f"--level2-toc={level2_toc}" ,f"--level3-toc={level3_toc}"]
-    for key, value in dict_arga.items():
+    for key, value in dict_args.items():
         args.append(f"--{key}={value}")
     subprocess.run(args,stdout=subprocess.DEVNULL,  # משתיק את הפלט
             stderr=subprocess.DEVNULL,  # משתיק את השגיאות
             check=True)
 
 start = False
+os.makedirs("html", exist_ok=True)
+os.makedirs("epub", exist_ok=True)
 all_books = sefaria_api.SefariaApi().table_of_contents()
 sorted_toc = utils.recursive_register_categories(all_books)
 lang = "hebrew"
@@ -35,17 +37,19 @@ book_dir = ' dir="rtl"' if lang == "hebrew" else ""
 for book in sorted_toc:
     print(book["en_title"])
     print(book["he_title"])
-    if not start:
-        if book["en_title"] == "Targum Sheni on Esther":
+    if book["en_title"] == "Targum Sheni on Esther":
             start = True
+    if not start:
         continue
     book_ins = get_from_sefaria.Book(book["en_title"], lang, book["he_title"], book["path"])
     get_book = book_ins.process_book()
     metadata = book_ins.get_metadata()
-    soup = BeautifulSoup(f'<html lang={lang[:2]}><head><title></title></head><body{book_dir}>{"".join(get_book)}</body></html>', "html.parser")
+    soup = BeautifulSoup(f'<html lang={lang[:2]}><head><title>{book["he_title"]}</title></head><body{book_dir}>{"".join(get_book)}</body></html>', "html.parser")
 
     soup = soup.prettify()
     book_name = utils.sanitize_filename(book["he_title"]) if lang == "hebrew" else utils.sanitize_filename(book["en_title"])
-    with open(f"{book_name}.html", "w", encoding="utf-8") as f:
+    file_name = os.path.join("html", f"{book_name}.html")
+    epub_name = os.path.join("epub", f"{book_name}.epub")
+    with open(file_name, "w", encoding="utf-8") as f:
         f.write(str(soup))
-    to_ebook(f"{book_name}.html", f"{book_name}.epub", metadata)
+    to_ebook(file_name, epub_name, metadata)
